d5895f70232cbf5d0a847b81de4cd04c472fd861
Patrick Bacon-Blaber <pablaber225@gmail.com>
2024-05-22 08:37:55 -0400

    fix: fixed broken types base file in build

--- lib/writer/types-base.ts
+++
@@ -1,4 +0,0 @@
-// This is the types base file, which contains some common code used throughout
-// the project. All types will be added below the contents of this file.
-
-type Nullable<T> = T | null;
